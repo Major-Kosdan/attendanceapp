26fbffa274973eb3fabc4021bf4574e576016632
Major-Kosdan <135410685+Major-Kosdan@users.noreply.github.com>
2024-12-09 18:42:18 +0100

    Add files via upload

---
+++ attendanceapp/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>LoginPage</title>
+</head>
+<body>
+    <div class="loginform">
+        <div class="inputgroup">
+            <input type="text" id="txtusername" required>
+            <label for="txtusername">Username</label>
+
+        </div>
+    </div>
+</body>
+</html>
